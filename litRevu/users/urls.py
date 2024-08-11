4e272957a59cdcbd58b1048a7b1e163b2e578da1
narjesb <narjes.boulila@gmail.com>
2024-08-11 18:39:26 +0200

    initial commit

---
+++ litRevu/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('home', views.home, name='home'),
+    path('signup/', views.signup_page, name='signup')
+]
---
+++ litRevu/users/views.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from . import forms
+
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = ''
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+        message = 'Identifiants invalides.'
+    return render(request, 'login.html', context={'form': form,
+                                                  'message': message})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required
+def home(request):
+    return redirect('home')
+
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # auto-login user
+            login(request, user)
+            return redirect('login')
+    return render(request, 'signup.html', context={'form': form})
